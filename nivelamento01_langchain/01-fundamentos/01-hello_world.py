f2f170bc73cec1f61253e17778d24e20859c8299
Vicente Cruz <vicruz81@yahoo.com.br>
2026-02-21 08:45:57 -0300

    hello world usando LLMs Gemini e ChatGPT

---
+++ nivelamento01_langchain/01-fundamentos/01-hello_world.py
@@ -0,0 +1,8 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-5-nano", temperature=0.5)
+message = model.invoke("Hello World")
+
+print(message.content)
